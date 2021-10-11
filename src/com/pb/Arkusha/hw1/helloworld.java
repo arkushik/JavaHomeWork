9600153589a77484e247d4531bbef2eabf82436f
Max Arkusha <maksim.arkusha@privatbank.ua>
2021-10-11 22:18:13 +0300

    init commit

--- src/com/pb/Arkusha/hw1/helloworld.java
+++ src/com/pb/Arkusha/hw1/helloworld.java
@@ -2,7 +2,7 @@ package com.pb.Arkusha.hw1;
 
 public class helloworld {
     public static void main(String[] args) {
-        System.out.println("Hello World22222");
+        System.out.println("Hello World");
     }
 
 }
