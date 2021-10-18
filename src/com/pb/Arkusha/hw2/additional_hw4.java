6931eb976d45fb91d577c390e3a021cf0410d51a
Max Arkusha <maksim.arkusha@privatbank.ua>
2021-10-18 21:00:55 +0300

    init commit

--- src/com/pb/Arkusha/hw2/additional_hw4.java
+++ src/com/pb/Arkusha/hw2/additional_hw4.java
@@ -1,3 +1,5 @@
+///определение минимального модуля чисел
+
 package com.pb.Arkusha.hw2;
 
 import java.util.Scanner;
@@ -18,13 +20,30 @@ public class additional_hw4 {
         System.out.print("Введите 3е число: ");
         operand3 = scan.nextInt();
         // char sign1 = sign.charAt(0);
-        System.out.println("Ваши числа - " + operand1 + "," + operand2+ "," + operand3);
+        System.out.println("Ваши числа : " + operand1 + "," + operand2+ "," + operand3);
 
         int operand11 = abs(operand1);
         int operand22 = abs(operand2);
         int operand33 = abs(operand3);
 
-        System.out.println("Ваши числа после модуля- " + operand11 + "," + operand22+ "," + operand33);
+        System.out.println("Ваши числа после модуля: " + operand11 + "," + operand22+ "," + operand33);
+
+        if (operand11<=operand22 && operand11<=operand33)
+        {
+            System.out.println(operand11);
+        }
+        else if
+        (operand22<=operand33 && operand22<=operand11)
+        {
+            System.out.println(operand22);
+        }
+        else if (operand33<=operand11 && operand33<=operand22)
+        {
+            System.out.println(operand33);
+        }
+        else {
+            System.out.println("Неправильное выражение");
+        }
 
     }
 }
