ace86dee0465571b76a11016578a2cd9a7b0f335
Max Arkusha <maksim.arkusha@privatbank.ua>
2021-10-18 20:20:58 +0300

    init commit

---
+++ src/com/pb/Arkusha/hw2/additional_hw4.java
@@ -0,0 +1,30 @@
+package com.pb.Arkusha.hw2;
+
+import java.util.Scanner;
+
+import static java.lang.Math.abs;
+
+public class additional_hw4 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int operand1, operand2, operand3;
+
+        System.out.print("Введите первое число: ");
+        operand1 = scan.nextInt();
+
+        System.out.print("Введите второе число: ");
+        operand2 = scan.nextInt();
+
+        System.out.print("Введите 3е число: ");
+        operand3 = scan.nextInt();
+        // char sign1 = sign.charAt(0);
+        System.out.println("Ваши числа - " + operand1 + "," + operand2+ "," + operand3);
+
+        int operand11 = abs(operand1);
+        int operand22 = abs(operand2);
+        int operand33 = abs(operand3);
+
+        System.out.println("Ваши числа после модуля- " + operand11 + "," + operand22+ "," + operand33);
+
+    }
+}
